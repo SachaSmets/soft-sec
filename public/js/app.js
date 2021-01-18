cd4b6a484b41a62f060ab5873b67c0909ba8144f
sacha smets <sacha.smets@student.ehb.be>
2021-01-18 14:17:07 +0100

    :lipstick: changed cookieconsent stylesheet

--- public/js/app.js
+++ public/js/app.js
@@ -21351,8 +21351,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /data/sites/web/sachasmetsbe/www/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /data/sites/web/sachasmetsbe/www/resources/css/app.css */"./resources/css/app.css");
+__webpack_require__(/*! D:\_GIT\soft_sec\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! D:\_GIT\soft_sec\resources\css\app.css */"./resources/css/app.css");
 
 
 /***/ })
